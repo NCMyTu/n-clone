f24272580e80b23871e9c7bb40e2f77c2d8b2c88
NCMyTu <145173051+NCMyTu@users.noreply.github.com>
2025-08-18 22:19:12 +0700

    Small test

--- db.py
+++ db.py
@@ -7,4 +7,16 @@ from sqlalchemy import create_engine, event, select
 
 dbm = DatabaseManager("sqlite:///collection.db.sqlite", echo=False)
 
-dbm.insert_parody("new_parody3")
+dbm.create_database()
+
+dbm.insert_doujinshi(1, "full_name", "path_1")
+
+dbm.insert_language("new_lang")
+
+dbm.add_language_to_doujinshi(1, "new_lang")
+
+d = dbm.get_doujinshi(1)
+
+print("Languages:")
+for lang in d.languages:
+	print("\t", lang.name)

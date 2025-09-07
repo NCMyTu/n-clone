6178a7b8a5120d595c516982ab97bac72299c26d
NCMyTu <145173051+NCMyTu@users.noreply.github.com>
2025-09-07 10:52:29 +0700

    Add DEFAULT 0 to count columns in item tables at db level

--- src/models/parody.py
+++ src/models/parody.py
@@ -4,7 +4,7 @@ from typing import List
 from .base import Base
 from .many_to_many_tables import doujinshi_parody
 from sqlalchemy import CheckConstraint, Column
-from sqlalchemy import Integer, Text
+from sqlalchemy import Integer, Text, text
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import relationship
@@ -15,7 +15,7 @@ class Parody(Base):
 
 	id: Mapped[int] = mapped_column(Integer, primary_key=True)
 	name: Mapped[str] = mapped_column(Text, nullable=False, unique=True)
-	count: Mapped[int] = mapped_column(Integer, default=0)
+	count: Mapped[int] = mapped_column(Integer, default=0, server_default=text("0"))
 
 	doujinshis: Mapped[list["Doujinshi"]] = relationship(
 		"Doujinshi",

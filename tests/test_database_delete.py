1f019df9f39692a847a259d28bc863c30b67eebc
NCMyTu <145173051+NCMyTu@users.noreply.github.com>
2025-09-13 20:25:31 +0700

    Rewrite test: db_delete and db_update

--- tests/test_database_delete.py
+++ tests/test_database_delete.py
@@ -1,12 +1,24 @@
 import pytest
 from src import DatabaseStatus
+import random
 
 
-def test_remove_doujinshi(dbm, sample_doujinshi):
-	assert dbm.insert_doujinshi(sample_doujinshi, False) == DatabaseStatus.OK
+def test_remove_doujinshi(dbm, sample_n_random_doujinshi):
+	doujinshi_list, _ = sample_n_random_doujinshi(50)
 
-	assert dbm.remove_doujinshi(sample_doujinshi["id"]) == DatabaseStatus.OK
-	assert dbm.remove_doujinshi(sample_doujinshi["id"]) == DatabaseStatus.NOT_FOUND
-	assert dbm.remove_doujinshi(-9999999) == DatabaseStatus.NOT_FOUND
+	assert dbm.remove_doujinshi(-999) == DatabaseStatus.NOT_FOUND
+	assert dbm.remove_doujinshi(10**9) == DatabaseStatus.NOT_FOUND
 
-# TODO: check count of items
+	for doujinshi in doujinshi_list:
+		dbm.insert_doujinshi(doujinshi)
+
+	random.shuffle(doujinshi_list)
+	for doujinshi in doujinshi_list:
+		assert dbm.remove_doujinshi(doujinshi["id"]) == DatabaseStatus.OK
+
+	random.shuffle(doujinshi_list)
+	for doujinshi in doujinshi_list:
+		assert dbm.remove_doujinshi(doujinshi["id"]) == DatabaseStatus.NOT_FOUND
+
+	assert dbm.remove_doujinshi(-999) == DatabaseStatus.NOT_FOUND
+	assert dbm.remove_doujinshi(10**9) == DatabaseStatus.NOT_FOUND
--- tests/test_database_update.py
+++ tests/test_database_update.py
@@ -1,11 +1,9 @@
 import pytest
 from src import DatabaseStatus
-from .utils import _sample_doujinshi
 import random
 
-# TODO: get rid of all update_count calls
 
-@pytest.mark.parametrize("add_method_name, insert_method_name, properti", [ # "property" is reserved...
+@pytest.mark.parametrize("add_method_name, insert_method_name, field", [
 	("add_parody_to_doujinshi", "insert_parody", "parodies"),
 	("add_character_to_doujinshi", "insert_character", "characters"),
 	("add_tag_to_doujinshi", "insert_tag", "tags"),
@@ -13,56 +11,58 @@ import random
 	("add_group_to_doujinshi", "insert_group", "groups"),
 	("add_language_to_doujinshi", "insert_language", "languages"),
 ])
-def test_add_item_to_doujinshi(dbm, sample_doujinshi, add_method_name, insert_method_name, properti):
-	assert dbm.insert_doujinshi(sample_doujinshi, False) == DatabaseStatus.OK
-
+def test_add_item_to_doujinshi(dbm, sample_doujinshi, add_method_name, insert_method_name, field):
+	dbm.insert_doujinshi(sample_doujinshi, False)
 	d_id = sample_doujinshi["id"]
-	add_to_doujinshi = getattr(dbm, add_method_name)
-	insert_into_db = getattr(dbm, insert_method_name)
+
+	add_item_to_doujinshi = getattr(dbm, add_method_name)
+	insert_item_into_db = getattr(dbm, insert_method_name)
 
 	new_items = ["new_item_1", "new_item_2", "new_item_3"]
 
 	# Check return statuses.
 	# Yes, those for loops need to be seperated like that to test "batch" operation.
 	for item in new_items:
-		assert add_to_doujinshi(d_id, item) == DatabaseStatus.NOT_FOUND
+		assert add_item_to_doujinshi(d_id, item) == DatabaseStatus.NOT_FOUND
 
 	for item in new_items:
-		assert insert_into_db(item) == DatabaseStatus.OK
+		insert_item_into_db(item)
 
 	for item in new_items:
-		assert add_to_doujinshi(d_id, item) == DatabaseStatus.OK
+		assert add_item_to_doujinshi(d_id, item) == DatabaseStatus.OK
 	for item in new_items:
-		assert add_to_doujinshi(d_id, item) == DatabaseStatus.ALREADY_EXISTS
+		assert add_item_to_doujinshi(d_id, item) == DatabaseStatus.ALREADY_EXISTS
 
 	for item in new_items:
-		assert add_to_doujinshi(-9999999, item) == DatabaseStatus.NOT_FOUND
+		assert add_item_to_doujinshi(-9999999, item) == DatabaseStatus.NOT_FOUND
 
 	# Verify again in the actual doujinshi
-	d = dbm.get_doujinshi(d_id)
-	assert d
+	retrieved_doujinshi = dbm.get_doujinshi(d_id)
 	for item in new_items:
-		assert item in d[properti].keys()
+		assert item in retrieved_doujinshi[field].keys()
 
 
 def test_add_pages_to_doujinshi(dbm, sample_doujinshi):
-	assert dbm.insert_doujinshi(sample_doujinshi, False) == DatabaseStatus.OK
-
+	dbm.insert_doujinshi(sample_doujinshi, False)
 	d_id = sample_doujinshi["id"]
+
 	new_pages = [f"new_page_{i}" for i in range(1, 200)]
 	assert dbm.add_pages_to_doujinshi(d_id, new_pages) == DatabaseStatus.OK
-	new_d = dbm.get_doujinshi(d_id)
-	assert new_d
-	assert new_d["pages"] == new_pages, "Old and new pages have different order."
 
+	retrieved_doujinshi = dbm.get_doujinshi(d_id)
+	assert retrieved_doujinshi["pages"] == new_pages, "Old and new pages have different order."
+
+	random.seed(2)
 	random.shuffle(new_pages)
 	assert dbm.add_pages_to_doujinshi(d_id, new_pages) == DatabaseStatus.OK
-	new_d = dbm.get_doujinshi(d_id)
-	assert new_d
-	assert new_d["pages"] == new_pages, "Old and new pages have different order."
+
+	retrieved_doujinshi = dbm.get_doujinshi(d_id)
+	assert retrieved_doujinshi["pages"] == new_pages, "Old and new pages have different order."
+
+	assert dbm.add_pages_to_doujinshi(d_id, []) == DatabaseStatus.OK
 
 
-@pytest.mark.parametrize("remove_method_name, insert_method_name, properti", [
+@pytest.mark.parametrize("remove_method_name, insert_method_name, field", [
 	("remove_parody_from_doujinshi", "insert_parody", "parodies"),
 	("remove_character_from_doujinshi", "insert_character", "characters"),
 	("remove_tag_from_doujinshi", "insert_tag", "tags"),
@@ -70,190 +70,111 @@ def test_add_pages_to_doujinshi(dbm, sample_doujinshi):
 	("remove_group_from_doujinshi", "insert_group", "groups"),
 	("remove_language_from_doujinshi", "insert_language", "languages"),
 ])
-def test_remove_item_from_doujinshi(dbm, sample_doujinshi, remove_method_name, insert_method_name, properti):
-	assert dbm.insert_doujinshi(sample_doujinshi, False) == DatabaseStatus.OK
+def test_remove_item_from_doujinshi(dbm, sample_doujinshi, remove_method_name, insert_method_name, field):
+	dbm.insert_doujinshi(sample_doujinshi, False)
+	d_id = sample_doujinshi["id"]
 
 	remove_method = getattr(dbm, remove_method_name)
 	insert_method = getattr(dbm, insert_method_name)
 
 	n_items_to_remove = 3
-	items_to_remove = sample_doujinshi[properti][:n_items_to_remove]
+	items_to_remove = sample_doujinshi[field][:n_items_to_remove]
 
-	d_id = sample_doujinshi["id"]
-	# Check return statuses
-	for item in ["non-existent-1", "non_existent_2"]:
+	# Remove non-existent items
+	for item in ["non-existent-1", "non_existent_2", "non existent 3"]:
 		assert remove_method(d_id, item) == DatabaseStatus.NOT_FOUND
+
+	# Remove items linked with doujinshi
 	for item in items_to_remove:
 		assert remove_method(d_id, item) == DatabaseStatus.OK
+
+	# Remove items from a non-existent doujinshi
 	for item in items_to_remove:
 		assert remove_method(-999999, item) == DatabaseStatus.NOT_FOUND
 
-	# Remove an item that doesn't belong to the doujinshi.
+	# Remove an item that isn't linked to doujinshi.
 	assert insert_method("new_item") == DatabaseStatus.OK
 	assert remove_method(d_id, "new_item") == DatabaseStatus.NOT_FOUND
 
 	# Verify again in the actual doujinshi
-	d = dbm.get_doujinshi(d_id)
-	assert d
+	retrieved_doujinshi = dbm.get_doujinshi(d_id)
 	for item in items_to_remove:
-		assert item not in d[properti].keys()
-	assert len(d[properti]) == len(sample_doujinshi[properti]) - n_items_to_remove
+		assert item not in retrieved_doujinshi[field].keys()
+	assert len(retrieved_doujinshi[field]) == len(sample_doujinshi[field]) - n_items_to_remove
 
 
 def test_remove_pages_from_doujinshi(dbm, sample_doujinshi):
-	assert dbm.insert_doujinshi(sample_doujinshi, False) == DatabaseStatus.OK
-
+	dbm.insert_doujinshi(sample_doujinshi, False)
 	d_id = sample_doujinshi["id"]
+
 	assert dbm.remove_pages_from_doujinshi(d_id) == DatabaseStatus.OK
 
-	# Verify again in the actual doujinshi
-	d = dbm.get_doujinshi(d_id)
-	assert d
-	assert not d["pages"]
+	# Verify in the actual doujinshi
+	retrieved_doujinshi = dbm.get_doujinshi(d_id)
+	assert not retrieved_doujinshi["pages"]
+
+	# Remove empty `pages` should be OK
+	assert dbm.remove_pages_from_doujinshi(d_id) == DatabaseStatus.OK
 
 
-@pytest.mark.parametrize("update_method_name, field", [
+@pytest.mark.parametrize("update_method_name, column_name", [
 	("update_full_name_of_doujinshi", "full_name"),
 	("update_full_name_original_of_doujinshi", "full_name_original"),
 	("update_pretty_name_of_doujinshi", "pretty_name"),
 	("update_pretty_name_original_of_doujinshi", "pretty_name_original"),
 	("update_note_of_doujinshi", "note"),
-	("update_path_of_doujinshi", "path"),
 ])
-def test_update_doujinshi_fields(dbm, sample_doujinshi, update_method_name, field):
-	assert dbm.insert_doujinshi(sample_doujinshi, False) == DatabaseStatus.OK
-
+@pytest.mark.parametrize("value, expected_status", [
+    ("new_column", DatabaseStatus.OK),
+    ("", DatabaseStatus.INTEGRITY_ERROR),
+    (" ", DatabaseStatus.INTEGRITY_ERROR),
+    (" \n\t  ", DatabaseStatus.INTEGRITY_ERROR),
+    ([], DatabaseStatus.INTEGRITY_ERROR),
+    ((), DatabaseStatus.INTEGRITY_ERROR),
+    (set(), DatabaseStatus.INTEGRITY_ERROR),
+    ({}, DatabaseStatus.INTEGRITY_ERROR),
+    (object(), DatabaseStatus.INTEGRITY_ERROR),
+])
+def test_update_doujinshi_column(dbm, sample_doujinshi, update_method_name, column_name, value, expected_status):
+	dbm.insert_doujinshi(sample_doujinshi, False)
 	d_id = sample_doujinshi["id"]
 
 	update_method = getattr(dbm, update_method_name)
 
-	new_value = f"updated_{field}"
-	assert update_method(d_id, new_value) == DatabaseStatus.OK
+	assert update_method(d_id, value) == expected_status
 
-	# Verify again
-	d = dbm.get_doujinshi(d_id)
-	assert d
-	assert d[field] == new_value
+	if expected_status == DatabaseStatus.OK:
+		# Verify again in the actual doujinshi
+		retrieved_doujinshi = dbm.get_doujinshi(d_id)
+		assert retrieved_doujinshi[column_name] == value
 
-	assert update_method(-999999, new_value) == DatabaseStatus.NOT_FOUND
-
-
-@pytest.mark.parametrize("item_type, update_count_method, use_bulk_update",
-[
-	("parodies", "update_count_of_parody", False),
-	("characters", "update_count_of_character", False),
-	("tags", "update_count_of_tag", False),
-	("artists", "update_count_of_artist", False),
-	("groups", "update_count_of_group", False),
-	("languages", "update_count_of_language", False),
-	# same as above but using update_count_of_all() instead
-	("parodies", None, True),
-	("characters", None, True),
-	("tags", None, True),
-	("artists", None, True),
-	("groups", None, True),
-	("languages", None, True),
-])
-def test_get_count_of_item_type(dbm, item_type, update_count_method, use_bulk_update):
-	items = [f"item_{i}" for i in range(20)]
-	item_count = {item: 0 for item in items}
-
-	get_count_of = f"get_count_of_{item_type}"
-	get_count_of = getattr(dbm, get_count_of)
-
-	for d_id in range(1, 41):
-		doujinshi = _sample_doujinshi(d_id)
-
-		n_items = random.randint(0, len(items)//2)
-		selected_items = random.sample(items, n_items)
-		doujinshi[item_type] = selected_items
-
-		for item in selected_items:
-			item_count[item] += 1
-
-		assert dbm.insert_doujinshi(doujinshi, False) == DatabaseStatus.OK
-
-	if use_bulk_update:
-		# dbm.update_count_of_all()
-		pass
-	else:
-		getattr(dbm, update_count_method)()
-
-	retrieved_item_count = get_count_of(items)
-	assert retrieved_item_count == item_count
-
-	empty_item_count = get_count_of([])
-	assert empty_item_count == {}
-
-	no_exist_item_count = get_count_of(["no_exist"])
-	assert no_exist_item_count == {"no_exist": 0}
+		# Update a non-existent doujinshi
+		assert update_method(-999999, value) == DatabaseStatus.NOT_FOUND
 
 
-def test_get_count_of_items_when_getting_and_removing_doujinshi(dbm):
-	def is_subdict(small, big):
-		return all(k in big and big[k] == v for k, v in small.items())
+def test_update_doujinshi_path(dbm, sample_n_random_doujinshi):
+	doujinshi_list, _ = sample_n_random_doujinshi(30)
 
-	n_items_per_type = 30
-	parody_count, character_count, tag_count, artist_count, group_count, language_count = [
-		{f"item_{i}": 0 for i in range(n_items_per_type)} for _ in range(6)
-	]
+	for doujinshi in doujinshi_list:
+		dbm.insert_doujinshi(doujinshi)
 
-	mapping = {
-		"parodies": parody_count,
-		"characters": character_count,
-		"tags": tag_count,
-		"artists": artist_count,
-		"groups": group_count,
-		"languages": language_count,
-	}
+	doujinshi_path_list = [d["path"] for d in doujinshi_list]
 
-	n_doujinshis = 40
-	n_doujinshis_to_remove = 10
+	for i, doujinshi in enumerate(doujinshi_list):
+		# Update to its own path
+		assert dbm.update_path_of_doujinshi(doujinshi["id"], doujinshi["path"]) == DatabaseStatus.OK
+		# Update to other doujinshi's path
+		duplicate_path = doujinshi_path_list[(i + 1) % len(doujinshi_path_list)]
+		assert dbm.update_path_of_doujinshi(doujinshi["id"], duplicate_path) == DatabaseStatus.INTEGRITY_ERROR
 
-	# Insert new doujinshis
-	for i in range(n_doujinshis):
-		doujinshi = _sample_doujinshi(i)
+	for doujinshi in doujinshi_list:
+		# Update to a completely new path
+		new_path = f"new_path_{doujinshi['id']}"
+		assert dbm.update_path_of_doujinshi(doujinshi["id"], new_path) == DatabaseStatus.OK
 
-		for field, item_count in mapping.items():
-			n_items = random.randint(0, n_items_per_type)
+		# Verify again in the actual doujinshi.
+		retrieved_doujinshi = dbm.get_doujinshi(doujinshi["id"])
+		assert retrieved_doujinshi["path"] == new_path
 
-			doujinshi[field] = random.sample(list(item_count.keys()), n_items)
-
-			for item in doujinshi[field]:
-				item_count[item] += 1
-
-		dbm.insert_doujinshi(doujinshi, False)
-
-	# dbm.update_count_of_all()
-
-	# Verify counts after insertion
-	for i in range(n_doujinshis):
-		doujinshi = dbm.get_doujinshi(i)
-		assert doujinshi
-
-		for field, item_count in mapping.items():
-			assert is_subdict(doujinshi[field], item_count)
-
-	# Manually update counts after removal
-	for i in range(n_doujinshis_to_remove):
-		doujinshi = dbm.get_doujinshi(i)
-		assert doujinshi
-
-		# Remove first n_doujinshis_to_remove doujinshis
-		if i < n_doujinshis_to_remove:
-			for field, item_count in mapping.items():
-				for item in doujinshi[field]:
-					item_count[item] -= 1
-
-			return_status = dbm.remove_doujinshi(i)
-			assert return_status == DatabaseStatus.OK
-
-	dbm.update_count_of_all()
-
-	# Verify again
-	for i in range(n_doujinshis_to_remove, n_doujinshis):
-		doujinshi = dbm.get_doujinshi(i)
-		assert doujinshi
-
-		for field, item_count in mapping.items():
-			assert is_subdict(doujinshi[field], item_count)
+	# Update path of a non-existent doujinshi
+	assert dbm.update_path_of_doujinshi(-999999, "new_path") == DatabaseStatus.NOT_FOUND
